b0d6fe36d5a3d46d456dc144bc6bf0bb53c76a70
Micah Pope <mpope316@gmail.com>
2019-10-06 13:24:34 -0400

    changed line endings, beautified indents, moved calendar script to external file, added config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+  API_KEY : 'api_key',
+  CLIENT_ID : 'client_id',
+  CALENDAR : 'calendarId can be found in the Google Calendar settings and looks like an email @group.calendar.google.com'
+}
