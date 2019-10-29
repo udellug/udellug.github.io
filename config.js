e92cd225aff5ae9e59fea037878f39be9fbdecc2
Ryan Geary <rtgnj42@gmail.com>
2019-10-29 17:53:22 -0400

    Merge pull request #1 from udellug/calendar

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+  API_KEY : 'AIzaSyB0WvjDMS--br-QZv9QwQHxIoechWY_3eU',
+  CLIENT_ID : '110394664397-6235eak4c78um1k9h8920mcr0l4brgjj.apps.googleusercontent.com',
+  CALENDAR : 'udellug@gmail.com'
+}
