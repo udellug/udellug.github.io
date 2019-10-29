de729a9bf1e5b84b8072bbe718228113fab463fc
Micah Pope <micah@micahpope.com>
2019-10-29 13:39:56 -0700

    Calendar script completed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-  API_KEY : 'AIzaSyBsAMC8fSrY8Xim5-UsGJ8DG1bJq9m0G50',
-  CLIENT_ID : '1025843156932-6utmhq13s1erfttcbsbk0vl821f86190.apps.googleusercontent.com',
-  CALENDAR : 'udelllug@gmail.com'
+  API_KEY : 'AIzaSyB0WvjDMS--br-QZv9QwQHxIoechWY_3eU',
+  CLIENT_ID : '110394664397-6235eak4c78um1k9h8920mcr0l4brgjj.apps.googleusercontent.com',
+  CALENDAR : 'udellug@gmail.com'
 }
